ba0e95e86db0c44e40e86c5b116286ae6f716202
gustavkkk <gustav0125@outlook.com>
2017-09-12 16:04:11 -0400

    Update install-cudnn.sh

--- caffe-ssd-offline/install-cudnn.sh
+++ caffe-ssd-offline/install-cudnn.sh
@@ -5,9 +5,14 @@ include_dir=/usr/local/include
 #cudnn_file_name=cudnn-7.0-linux-x64-v4.0-prod.tgz
 #cudnn_lib_file_name=libcudnn.so.4.0.7
 #cuddn_ln_file_name=libcudnn.so.4
-cudnn_file_name=cudnn-8.0-linux-x64-v5.1.tgz
-cudnn_lib_file_name=libcudnn.so.5.1.5
-cuddn_ln_file_name=libcudnn.so.5
+
+#cudnn_file_name=cudnn-8.0-linux-x64-v5.1.tgz
+#cudnn_lib_file_name=libcudnn.so.5.1.5
+#cuddn_ln_file_name=libcudnn.so.5
+
+cudnn_file_name=cudnn-8.0-linux-x64-v6.0.tgz
+cudnn_lib_file_name=libcudnn.so.6.0.21
+cuddn_ln_file_name=libcudnn.so.6
 
 tar xvf $cudnn_file_name
 cd cuda/include
